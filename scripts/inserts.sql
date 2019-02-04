3803bb70639e011fa6e2562c6e27d71b1aa2c4e3
rponciano <romuloponciano@id.uff.br>
2019-02-04 14:03:39 -0200

    Criada v0 do script para inserts de usuários.

---
+++ scripts/inserts.sql
@@ -0,0 +1,7 @@
+INSERT INTO Status (id, nome) VALUES (0, "INATIVO");
+INSERT INTO Status (id, nome) VALUES (1, "ATIVO");
+
+INSERT INTO Usuario (id, login, nome, status, gerenciaAtual) VALUES (0, "usr1", "Primeiro Usuário", 1, "gerencia x");
+INSERT INTO Usuario (id, login, nome, status, gerenciaAtual) VALUES (1, "usr2", "Segundo Usuário", 0, "gerencia x");
+INSERT INTO Usuario (id, login, nome, status, gerenciaAtual) VALUES (2, "usr3", "Terceiro Usuário", 0, "gerencia y");
+INSERT INTO Usuario (id, login, nome, status, gerenciaAtual) VALUES (3, "usr4", "Quarto Usuário", 1, "gerencia z");
