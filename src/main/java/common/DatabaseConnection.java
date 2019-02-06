46aec2045079da0f8da78f4cae258c51b27e6e2c
rponciano <romuloponciano@id.uff.br>
2019-02-06 13:00:19 -0200

    corrigindo bug na classe de conexão do banco, pois a conexão não fechava corretamente.

--- src/main/java/common/DatabaseConnection.java
+++ src/main/java/common/DatabaseConnection.java
@@ -4,59 +4,45 @@ import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DatabaseConnection implements Serializable {
 	private static final long serialVersionUID = 6153988507264596623L;
 	
 	// vars
-    Connection connection; 
-    Statement statement;
+    Connection connection;
     
     /**
      * Ao criar uma instância desta classe, você esta 
-     * abrindo uma conexão com o banco. 
+     * tentando abrir uma NOVA conexão com o banco. 
      * @throws ClassNotFoundException se erro ao registrar JDBC
      * @throws SQLException se erro ao criar conexão com o banco
      */
     public DatabaseConnection() throws ClassNotFoundException, SQLException {
     	// registrar driver JDBC
     	Class.forName(Const.JDBC_DRIVER);
-    	// abrir conexão
-		this.connection = DriverManager.getConnection(Const.DB_URL, Const.USER, Const.PASS);
-    	this.statement = null;
+    	// conectar
+    	this.getConnection();
     };
     
     /**
-     * pegar conexão da instância conectada  
+     * pegar conexão da instância conectada ou criar nova conexão, se não existir 
+     * @throws SQLException 
      */
-    public Connection getConnection() {
+    public Connection getConnection() throws SQLException {
+    	if(this.connection == null) {
+    		this.connection = DriverManager.getConnection(Const.DB_URL, Const.USER, Const.PASS);
+    	}
     	return this.connection;
     }
     
-    
-    
     /**
-     * Método para pegar statement da instância de conexão 
-     * @return novo statement (se null) ou statement corrente
-     * @throws SQLException se erro ao criar novo statement
+     * fechar conexão da instância conectada e setar connection para null
      */
-    public Statement getStatement() throws SQLException {
-    	if(this.statement == null) {
-    		this.statement = this.connection.createStatement();
-    	};
-    	return statement;
-    };
-    
-    /**
-     * Método para fechar statement corrente da instância de conexão
-     * @throws SQLException se erro ao fechar statement não nulo
-     */
-    public void closeStatement() throws SQLException {
-    	if (this.statement != null) {
-            this.statement.close();        
-            this.statement = null;
-        };
-    };
+    public void closeConnection() {
+    	try {
+    		this.connection.close();
+    		this.connection = null; 
+    	} catch (Exception e) { /* ignored */ }
+    }
     
 }
